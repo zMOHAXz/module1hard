d36f8164cc84b2d36a5b5ad0b58822971c1de163
zMOHAXz <vlastorz@gmail.com>
2024-06-20 23:38:22 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,9 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+a=(sum(grades[0]))/len(grades[0])
+b=(sum(grades[1]))/len(grades[1])
+j=(sum(grades[2]))/len(grades[2])
+k=(sum(grades[3]))/len(grades[3])
+s=(sum(grades[4]))/len(grades[4])
+x=dict([["Aaron",a],["Bilbo",b],["Jonny",j],["Khendrik",k],["Stive",s]])
+print(x)
